ae0c2d1befb8edd8945e4dc306d69efc4a0d48a4
AlexVUlyanov <Ulianov2@mail.ru>
2022-06-04 21:12:22 +0300

    add PhaseA_PhaseB, PhaseB_PhaseC waveforms foto

--- 3-Phase_Sine_inverter/Clock2500Hz.v
+++ 3-Phase_Sine_inverter/Clock2500Hz.v
@@ -4,17 +4,17 @@ module Clock2500Hz
 	output outclok_dev
 );
 
-reg [15:0]count = 0;
+reg [19:0]count = 0;
 
 always @(posedge clk)
 begin
 	count <= count + 1'b1;
-	if (count >= (20000-1))
+	if (count >= (10000-1))
 	begin
 		count <= 0;
 	end
 end
 
-assign outclok_dev = (count < 10000) ? 1'b0:1'b1;
+assign outclok_dev = (count == 5000) ? 1'b0:1'b1;
 
 endmodule
