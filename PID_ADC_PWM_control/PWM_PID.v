bd81d980d642a7cbc080bc186c5ef62f51936a0a
Alex Ulyanov <Ulianov2@mail.ru>
2022-07-23 12:39:55 +0300

    add ADC to PWM (in 3V out 11 % PWM (3841-ADC code)) - test Ok, SignalTap - add info *bmp

---
+++ PID_ADC_PWM_control/PWM_PID.v
@@ -0,0 +1,120 @@
+module PWM_PID #(parameter DataTopValue = 32769, DataWidth = 15) 
+(  output  PWM_outP, 
+	output  PWM_outN,
+	input  [DataWidth-1:0]data, 
+	input clk_in,
+	input enable,
+	input reset);
+
+wire clk_dev;
+reg [DataWidth-1:0] cnt;
+
+reg temp_PWM_outP;
+reg temp_PWM_outN;
+
+binary_counter_PID (.clk(clk_in), .outclok_dev(clk_dev));
+
+always @(posedge clk_dev)
+begin
+
+	cnt <= cnt + 1;
+	
+	if (cnt >= DataTopValue )
+		begin
+			cnt <= 0;
+		end
+end
+
+	always @(*)
+		begin
+			if (!enable || reset)
+			begin
+					temp_PWM_outP = 1'b0;
+					temp_PWM_outN = 1'b0;
+			end
+				else
+				begin
+								if (data > cnt)
+								begin
+										temp_PWM_outP = 1'b1;
+										temp_PWM_outN = ~temp_PWM_outP;
+								end
+									else
+									begin
+										temp_PWM_outP = 1'b0;
+										temp_PWM_outN = ~temp_PWM_outP;
+									end
+				end
+		end
+
+wire delay_PWM_outP;
+wire delay_PWM_outN;
+
+delay_N_clock_shift_reg_PID #(.N(35)) delay_P_PWM_chanal (.in(temp_PWM_outP), .clk(clk_in), .reset(1'b1), .out(delay_PWM_outP));
+delay_N_clock_shift_reg_PID #(.N(35)) delay_N_PWM_chanal(.in(temp_PWM_outN), .clk(clk_in), .reset(1'b1), .out(delay_PWM_outN));
+
+////add deadTime out 680ns, PWM - 1.526Hz////
+
+assign PWM_outP = temp_PWM_outP & delay_PWM_outP;
+assign PWM_outN = temp_PWM_outN & delay_PWM_outN;
+
+endmodule
+
+module binary_counter_PID
+(
+	input clk,
+	output outclok_dev
+);
+
+reg [14:0]count = 0;
+
+always @(posedge clk)
+begin
+	count <= count + 1'b1;
+	if (count >= (2-1))
+	begin
+		count <= 0;
+	end
+end
+
+assign outclok_dev = (count < 1) ? 1'b0:1'b1; // 171
+
+endmodule
+
+//////////////////DeadTime controle//////////////
+
+module delay_N_clock_shift_reg_PID 
+#(parameter N=35)
+( 
+	input in, clk, reset,
+	output out
+);
+
+right_shift_register_PID #(.N(N)) (.MSB_in(in), .clk(clk), .reset(reset), .LSB_out(out));
+
+endmodule
+
+
+module right_shift_register_PID 
+#(parameter N=4)
+(
+	input MSB_in,
+	input clk,reset,
+	output reg [N-1:0] Data_out,
+	output LSB_out
+);
+
+assign LSB_out = Data_out[0];
+
+always @(posedge clk)
+begin
+	if (~reset)
+		begin
+			Data_out <= 0;
+		end
+			else
+			begin
+				Data_out <= {MSB_in, Data_out[N-1:1]};
+			end
+end
+endmodule
---
+++ PID_ADC_PWM_control/top_control.v
@@ -0,0 +1,97 @@
+module top_control ( 
+input  CLOCK_50,              // 50 MHz Clock ref
+input  ADC_SDAT,              // SPI ADC MISO
+output ADC_CS_N,              // SPI ADC nCS
+output ADC_SADDR,             // SPI ADC MOSI
+output ADC_SCLK,              // SPI ADC SCK
+output GPIO_00,               // PID out PWM
+output [7:0] LED
+);
+
+
+
+
+//////-----ADC-SPI-----//////
+wire ClkDev;
+wire [7:0] in_adc_chanel_read = 8'b0;         // (default) IN0
+wire o_RX_DV;
+wire o_TX_Ready;
+wire [7:0] adc_RX_Byte;
+
+reg [7:0] count_byte  = 0;
+reg [7:0] rx_byte_MSB = 0;
+reg [7:0] rx_byte_LSB = 0;
+
+assign ADC_CS_N = 1'b0;
+clock_divider #(.M(4)) (.clock_out(ClkDev), .clk_in(CLOCK_50));
+
+spi_master
+#(.SPI_MODE(0), .CLKS_PER_HALF_BIT(4))
+  (
+   // Control/Data Signals,
+   .i_Rst_L(1'b1),                          // FPGA Reset
+   .i_Clk(CLOCK_50),                        // FPGA Clock
+   
+   // TX (MOSI) Signals
+   .i_TX_Byte(in_adc_chanel_read),          // Byte to transmit on MOSI
+   .i_TX_DV(ClkDev),                        // Data Valid Pulse with i_TX_Byte
+   .o_TX_Ready(o_TX_Ready),                 // Transmit Ready for next byte
+   
+   // RX (MISO) Signals
+   .o_RX_DV(o_RX_DV),                       // Data Valid pulse (1 clock cycle)
+   .o_RX_Byte(adc_RX_Byte),                 // Byte received on MISO
+
+   // SPI Interface
+   .o_SPI_Clk(ADC_SCLK),
+   .i_SPI_MISO(ADC_SDAT),
+   .o_SPI_MOSI(ADC_SADDR)
+   );
+
+/// --- RX MSB and LSB byte --- ///
+always @(posedge ADC_SCLK)
+begin
+count_byte = count_byte + 1;
+	if (count_byte == 1)
+	begin
+		rx_byte_MSB <= adc_RX_Byte;
+	end
+		if (count_byte == 2)
+		begin
+			rx_byte_LSB <= adc_RX_Byte;
+			count_byte = 0;
+		end
+end
+
+wire [7:0] w_rx_byte_MSB;
+wire [7:0] W_rx_byte_LSB;
+
+assign w_rx_byte_MSB = rx_byte_MSB;
+assign w_rx_byte_LSB = rx_byte_LSB;
+
+reg  [11:0] ADC_Data;
+wire [11:0] w_ADC_DAta;
+
+assign w_ADC_DAta = ADC_Data;
+
+//// --- function calc ADC data 12-bit --- ////
+function [11:0] temp_ADC_data (input [7:0] MSB, LSB);
+temp_ADC_data = {MSB,LSB};   // or ((MSB<<8)|LSB);
+endfunction 
+
+/// --- 12-bit data ADC --- ///
+always @(posedge CLOCK_50)
+begin
+	ADC_Data <= temp_ADC_data(w_rx_byte_MSB,w_rx_byte_LSB);
+end
+
+//////----PWM-OUT----///////
+PWM_PID #(.DataTopValue(32767), .DataWidth(15)) (  
+.PWM_outP(GPIO_00), 
+.data(w_ADC_DAta), 
+.clk_in(CLOCK_50),
+.enable(1'b1),
+.reset(1'b0));
+
+assign LED[0] = GPIO_00; // real PWM out LED indicate
+
+endmodule 
