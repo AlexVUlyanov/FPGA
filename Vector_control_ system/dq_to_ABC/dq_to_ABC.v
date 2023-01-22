c32ea7e99772ef8d188c2713592743bfda555b5d
Alex Ulyanov <Ulianov2@mail.ru>
2023-01-22 12:13:40 +0300

    add verilog dq to ABC module

--- Vector_control_ system/dq_to_ABC/dq_to_ABC.v
+++ Vector_control_ system/dq_to_ABC/dq_to_ABC.v
@@ -1,15 +1,52 @@
 module dq_to_ABC (
 
-input [23:0] CosQ,
-input [23:0] SinQ,
-input [23:0] d,
-input [23:0] q,
+input  [23:0] CosQ,
+input  [23:0] SinQ,
+input  [23:0] d,
+input  [23:0] q,
 
 output [23:0] A,
 output [23:0] B,
 output [23:0] C
 
 );
+
+wire [23:0] w_mul_1;
+wire [23:0] w_mul_2;
+wire [23:0] w_mul_3;
+wire [23:0] w_mul_4;
+
+qmult_ mul_1 (.i_multiplicand(CosQ), .i_multiplier(d), .o_result(w_mul_1));  
+qmult_ mul_2 (.i_multiplicand(SinQ), .i_multiplier(q), .o_result(w_mul_2)); 
+qmult_ mul_3 (.i_multiplicand(CosQ), .i_multiplier(q), .o_result(w_mul_3));
+qmult_ mul_4 (.i_multiplicand(SinQ), .i_multiplier(d), .o_result(w_mul_4)); 
+
+
+wire [23:0] w_add_1;
+wire [23:0] w_add_2;
+
+qadd_ add_1 (.a(w_mul_1), .b(w_mul_2), .c(w_add_1));
+qadd_ add_2 (.a(w_mul_3), .b(w_mul_4), .c(w_add_2));
+
+wire [23:0] const1 = 23'b000000000000_100000000000; // 0.5
+wire [23:0] const2 = 23'b000000000000_110111011011; // 0.8660254
+
+wire [23:0] w_mul_5;
+wire [23:0] w_mul_6;
+
+qmult_ mul_5 (.i_multiplicand(w_add_1), .i_multiplier(const1), .o_result(w_mul_5));  
+qmult_ mul_6 (.i_multiplicand(w_add_2), .i_multiplier(const2), .o_result(w_mul_6));
+
+wire [23:0] w_add_3;
+wire [23:0] w_add_4;
+
+qadd_ add_3 (.a(w_mul_6), .b(w_mul_5), .c(w_add_3));
+qadd_ add_4 (.a(w_mul_6), .b(w_mul_5), .c(w_add_4));
+
+assign A = w_add_1;
+assign B = w_add_3;
+assign C = w_add_4;
+
 
 endmodule
 
